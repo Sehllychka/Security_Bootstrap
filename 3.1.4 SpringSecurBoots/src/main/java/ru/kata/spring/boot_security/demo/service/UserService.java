a98ca56c844fac4858319806a95e27d95af2b607
Sehllychka <141375328+Sehllychka@users.noreply.github.com>
2024-02-24 04:52:12 +0300

    Update UserService.java

--- 3.1.4 SpringSecurBoots/src/main/java/ru/kata/spring/boot_security/demo/service/UserService.java
+++ 3.1.4 SpringSecurBoots/src/main/java/ru/kata/spring/boot_security/demo/service/UserService.java
@@ -1,14 +1,11 @@
 package ru.kata.spring.boot_security.demo.service;
 
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import ru.kata.spring.boot_security.demo.model.User;
 
 import java.util.List;
 
 
-public interface UserService extends UserDetailsService {
+public interface UserService {
     List<User> getUsers();
 
     void addUser(User user);
@@ -16,7 +13,4 @@ public interface UserService extends UserDetailsService {
     void removeUser(long id);
 
     void updateUser(User user);
-
-    @Override
-    UserDetails loadUserByUsername(String s) throws UsernameNotFoundException;
 }
