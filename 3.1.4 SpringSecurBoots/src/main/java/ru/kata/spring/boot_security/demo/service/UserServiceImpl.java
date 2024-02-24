c8dc3fd8a60c08195ee085d2d53653769c64c7ec
Sehllychka <141375328+Sehllychka@users.noreply.github.com>
2024-02-24 04:52:31 +0300

    Update UserServiceImpl.java

--- 3.1.4 SpringSecurBoots/src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImpl.java
+++ 3.1.4 SpringSecurBoots/src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImpl.java
@@ -1,8 +1,6 @@
 package ru.kata.spring.boot_security.demo.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -45,14 +43,5 @@ public class UserServiceImpl implements UserService {
     public void updateUser(User user) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         userDao.updateUser(user);
-    }
-
-    @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        User user = userDao.getUserByUsername(email);
-        if (user == null) {
-            throw new UsernameNotFoundException(String.format("Пальзователь %s не найде!", email));
-        }
-        return user;
     }
 }
